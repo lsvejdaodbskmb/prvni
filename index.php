6b2cbb8bce0b6dfe2419a67f856ffbb0e22d5de7
Libor Švejda <libor.svejda@odbskmb.local>
2020-02-12 15:34:11 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <title>Prvni stranka pomoci PHPka</title>
+</head>
+<body>
+
+<h1>Prvni stranka pomoci PHPka</h1>
+
+<?php
+$c1 = 5;
+$c2 = 78;
+$vysl = $c1 + $c2;
+echo "Vysledek souctu $c1 a $c2 je $vysl!";
+?>
+
+<h2>další část...</h2>
+
+<?php
+$jm = "Bob";
+$pr = "Bobickovic";
+$roknar = 2002;
+echo "Prihlaseny uzivatel: $jm $pr ($roknar)";
+?>
+
+</body>
+</html>
+
---
+++ vzdalsluzba.php
@@ -0,0 +1,4 @@
+<?php
+header("Content-Type: application/json");
+
+echo '{"jmeno":"Bob","prijmeni":"Bobicek","roknar":2002}';
